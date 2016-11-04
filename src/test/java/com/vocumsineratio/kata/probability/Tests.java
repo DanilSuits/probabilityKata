48859066c2868c21e16896f4fb644148f61e98d5
Danil <danil@vast.com>
2016-11-04 13:34:57 -0500

    Begin the kata: https://gist.github.com/gregoryyoung/1018570
    This begins my third session; here I'm trying to better apply
    better TDD techniques, as described here:
    http://cascadefaliure.blogspot.com/2017/07/tdd-and-immutable-tests.html

---
+++ src/test/java/com/vocumsineratio/kata/probability/Tests.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright Vast 2016. All Rights Reserved.
+ * <p/>
+ * http://www.vast.com
+ */
+package com.vocumsineratio.kata.probability;
+
+/**
+ * @author Danil Suits (danil@vast.com)
+ */
+public class Tests {
+
+}
