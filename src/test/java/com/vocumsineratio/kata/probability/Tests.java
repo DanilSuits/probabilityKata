ce12af782b50e4aac47f194cb8de3fd3ee5e290b
Danil <danil@vast.com>
2016-11-04 13:41:34 -0500

    Introducing the problem statement.

--- src/test/java/com/vocumsineratio/kata/probability/Tests.java
+++ src/test/java/com/vocumsineratio/kata/probability/Tests.java
@@ -9,5 +9,22 @@ package com.vocumsineratio.kata.probability;
  * @author Danil Suits (danil@vast.com)
  */
 public class Tests {
+    /*
+        Write a probability value object. It should contain the following methods:
+           Probability CombinedWith(Probability)
+           Probability InverseOf()
+           Probability Either(Probability)
 
+        if you forget your probability math:
+           Either:P(A) + P(B) - P(A)P(B)
+           CombinedWith: P(A)P(B)
+           InverseOf: 1 - P(A)
+
+        The math is surprisingly not the main part of the exercise.
+
+        The internal state should be held as a decimal. Use TDD when writing this object.
+
+        One last thing. You can't put any getters on the object.  What's the first test to write?
+
+    */
 }
