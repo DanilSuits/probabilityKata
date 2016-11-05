f70bd352e1fb629160e1bbfe70493bfc9c7b5739
Danil <danil@vast.com>
2016-11-05 16:25:10 -0500

    GREEN Porting the earlier DeMorgan test
    which passes, of course, thanks to these stateless probability objects

--- src/test/java/com/vocumsineratio/kata/probability/TestSpikeTwo.java
+++ src/test/java/com/vocumsineratio/kata/probability/TestSpikeTwo.java
@@ -6,7 +6,12 @@
 package com.vocumsineratio.kata.probability;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * @author Danil Suits (danil@vast.com)
@@ -31,27 +36,64 @@ public class TestSpikeTwo {
 
     */
 
-    @Test
-    public void checkThatProbablityImplementsCorrectSignatures() {
-        Probability p = new Probability();
+    @Test(dataProvider = "unaryProvider")
+    public void checkThatProbablityImplementsCorrectSignatures(Probability p) {
         Probability q = p.inverseOf();
         Probability r = p.either(q);
         Probability s = p.combinedWith(q);
     }
 
-    @Test
-    public void checkThatNegationIfNegationIsIdentity () {
-        Probability p = new Probability();
+    @Test(dataProvider = "unaryProvider")
+    public void checkThatNegationIfNegationIsIdentity(Probability p) {
         Assert.assertEquals(p.inverseOf().inverseOf(), p);
+    }
 
-        Probability q = new Probability();
-        Assert.assertEquals(q.inverseOf().inverseOf(), q);
-
+    @Test(dataProvider = "binaryProvider")
+    public void checkThatNegationOfNegationIsIdentity(Probability p, Probability q) {
         Probability r = p.either(q);
         Assert.assertEquals(r.inverseOf().inverseOf(), r);
 
         Probability s = p.combinedWith(q);
         Assert.assertEquals(s.inverseOf().inverseOf(), s);
+    }
+
+    @Test(dataProvider = "binaryProvider")
+    public void checkThatDeMorgansLawsAreSatisified(Probability p, Probability q) {
+        {
+            // The negation of the disjunction is the conjunction of the negations
+            Probability disjunction = p.combinedWith(q);
+            Probability negationOfDisjunction = disjunction.inverseOf();
+
+            Probability notP = p.inverseOf();
+            Probability notQ = q.inverseOf();
+            Probability conjunctionOfNegations = notP.either(notQ);
+
+            Assert.assertEquals(negationOfDisjunction, conjunctionOfNegations);
+        }
+
+        {
+            // The negation of the conjunction is the disjunction of the negations
+            Probability conjunction = p.either(q);
+            Probability negationOfTheConjunction = conjunction.inverseOf();
+
+            Probability notP = p.inverseOf();
+            Probability notQ = q.inverseOf();
+            Probability disjunctionOfNegations = notP.combinedWith(notQ);
+
+            Assert.assertEquals(negationOfTheConjunction, disjunctionOfNegations);
+        }
+    }
+
+    @DataProvider
+    public Iterator<Object[]> unaryProvider() {
+        List<Object[]> trials = Arrays.<Object[]>asList(new Object[]{new Probability()});
+        return trials.iterator();
+    }
+
+    @DataProvider
+    public Iterator<Object[]> binaryProvider() {
+        List<Object[]> trials = Arrays.<Object[]>asList(new Object[]{new Probability(), new Probability()});
+        return trials.iterator();
     }
 
     static class Probability {
