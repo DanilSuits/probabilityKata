efa66f30d71ce061ade9821c1785ee14e88a6055
Danil <danil@vast.com>
2016-11-05 13:16:03 -0500

    GREEN: empty test harness

---
+++ src/test/java/com/vocumsineratio/kata/probability/TestSpikeTwo.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright Vast 2016. All Rights Reserved.
+ * <p/>
+ * http://www.vast.com
+ */
+package com.vocumsineratio.kata.probability;
+
+/**
+ * @author Danil Suits (danil@vast.com)
+ */
+public class TestSpikeTwo {
+    /*
+        Write a probability value object. It should contain the following methods:
+           Probability CombinedWith(Probability)
+           Probability InverseOf()
+           Probability Either(Probability)
+
+        if you forget your probability math:
+           Either:P(A) + P(B) - P(A)P(B)
+           CombinedWith: P(A)P(B)
+           InverseOf: 1 - P(A)
+
+        The math is surprisingly not the main part of the exercise.
+
+        The internal state should be held as a decimal. Use TDD when writing this object.
+
+        One last thing. You can't put any getters on the object.  What's the first test to write?
+
+    */
+}
